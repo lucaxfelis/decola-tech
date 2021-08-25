21ca53fce822b1bb524c384a610f0a692b38347f
Lucas Felix <lucasfelixaguiar@gmail.com>
2021-08-25 13:09:25 -0300

    feat: adicionar soluções do Primeiros desafios Matemáticos em C#

---
+++ primeiros-desafios-matematicos-csharp/DDD/DDD.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace DDD
+{
+    class DDD 
+    {
+
+        static void Main(string[] args) 
+        {  
+            // criando variável inteira
+            // convertendo entrada em string para inteiro
+            int ddd = Convert.ToInt32(Console.ReadLine());
+
+            // implentação do switch case
+            switch (ddd)
+            {
+                case 61:
+                    Console.WriteLine("Brasilia");
+                    break;
+                case 71:                             
+                    Console.WriteLine("Salvador");
+                    break;
+                case 11:
+                    Console.WriteLine("Sao Paulo");
+                    break;
+                case 21:
+                    Console.WriteLine("Rio de Janeiro");
+                    break;
+                case 32:
+                    Console.WriteLine("Juiz de Fora");
+                    break;
+                case 19:
+                    Console.WriteLine("Campinas");
+                    break;
+                case 27:
+                    Console.WriteLine("Vitoria");
+                    break;
+                case 31:
+                    Console.WriteLine("Belo Horizonte");
+                    break;
+                // caso padrão
+                default:
+                    Console.WriteLine("DDD nao cadastrado");
+                    break;
+            }
+        }
+    }
+}
---
+++ primeiros-desafios-matematicos-csharp/SomaSimples/SomaSimples.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SomaSimples
+{
+    class SomaSimples
+    {
+        static void Main(string[] args)
+        {
+            // declarando as variáveis
+            // convertendo os valores de string para inteiro
+            int A = Convert.ToInt32(Console.ReadLine());
+            int B = Convert.ToInt32(Console.ReadLine());
+        
+            // criando a variável soma e somando os valores
+            int SOMA = A + B;
+
+            // escrevendo resultado na tela com interpolação de string
+            Console.WriteLine($"SOMA = {SOMA}");
+        }
+    }
+}
