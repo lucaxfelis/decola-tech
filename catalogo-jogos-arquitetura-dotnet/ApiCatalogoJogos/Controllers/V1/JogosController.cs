69d2bc9b80362bc5c6dddf27da024b421fd0471d
Lucas Felix <lucasfelixaguiar@gmail.com>
2021-09-14 20:30:53 -0300

    feat: adicionar projeto até parte 5

--- catalogo-jogos-arquitetura-dotnet/ApiCatalogoJogos/Controllers/V1/JogosController.cs
+++ catalogo-jogos-arquitetura-dotnet/ApiCatalogoJogos/Controllers/V1/JogosController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using ApiCatalogoJogos.InputModel;
@@ -9,13 +10,12 @@ using ApiCatalogoJogos.ViewModel;
 using ApiCatalogoJogos.Services;
 using ApiCatalogoJogos.Exceptions;
 
-namespace ApiCatalogoJogos.Controllers.V1
+namespace ExemploApiCatalogoJogos.Controllers.V1
 {
-    [Route("api/V1/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class JogosController : ControllerBase
     {
-
         private readonly IJogoService _jogoService;
 
         public JogosController(IJogoService jogoService)
@@ -23,8 +23,18 @@ namespace ApiCatalogoJogos.Controllers.V1
             _jogoService = jogoService;
         }
 
+        /// <summary>
+        /// Buscar todos os jogos de forma paginada
+        /// </summary>
+        /// <remarks>
+        /// Não é possível retornar os jogos sem paginação
+        /// </remarks>
+        /// <param name="pagina">Indica qual página está sendo consultada. Mínimo 1</param>
+        /// <param name="quantidade">Indica a quantidade de reistros por página. Mínimo 1 e máximo 50</param>
+        /// <response code="200">Retorna a lista de jogos</response>
+        /// <response code="204">Caso não haja jogos</response>   
         [HttpGet]
-        public async Task<ActionResult<List<JogoViewModel>>> Obter([FromQuery, Range(1, int.MaxValue)] int pagina = 1, [FromQuery, Range(1, 50)] int quantidade = 5)
+        public async Task<ActionResult<IEnumerable<JogoViewModel>>> Obter([FromQuery, Range(1, int.MaxValue)] int pagina = 1, [FromQuery, Range(1, 50)] int quantidade = 5)
         {
             var jogos = await _jogoService.Obter(pagina, quantidade);
 
@@ -34,6 +44,12 @@ namespace ApiCatalogoJogos.Controllers.V1
             return Ok(jogos);
         }
 
+        /// <summary>
+        /// Buscar um jogo pelo seu Id
+        /// </summary>
+        /// <param name="idJogo">Id do jogo buscado</param>
+        /// <response code="200">Retorna o jogo filtrado</response>
+        /// <response code="204">Caso não haja jogo com este id</response>   
         [HttpGet("{idJogo:guid}")]
         public async Task<ActionResult<JogoViewModel>> Obter([FromRoute] Guid idJogo)
         {
@@ -45,6 +61,12 @@ namespace ApiCatalogoJogos.Controllers.V1
             return Ok(jogo);
         }
 
+        /// <summary>
+        /// Inserir um jogo no catálogo
+        /// </summary>
+        /// <param name="jogoInputModel">Dados do jogo a ser inserido</param>
+        /// <response code="200">Caso o jogo seja inserido com sucesso</response>
+        /// <response code="422">Caso já exista um jogo com mesmo nome para a mesma produtora</response>   
         [HttpPost]
         public async Task<ActionResult<JogoViewModel>> InserirJogo([FromBody] JogoInputModel jogoInputModel)
         {
@@ -60,6 +82,13 @@ namespace ApiCatalogoJogos.Controllers.V1
             }
         }
 
+        /// <summary>
+        /// Atualizar um jogo no catálogo
+        /// </summary>
+        /// /// <param name="idJogo">Id do jogo a ser atualizado</param>
+        /// <param name="jogoInputModel">Novos dados para atualizar o jogo indicado</param>
+        /// <response code="200">Cao o jogo seja atualizado com sucesso</response>
+        /// <response code="404">Caso não exista um jogo com este Id</response>   
         [HttpPut("{idJogo:guid}")]
         public async Task<ActionResult> AtualizarJogo([FromRoute] Guid idJogo, [FromBody] JogoInputModel jogoInputModel)
         {
@@ -75,6 +104,13 @@ namespace ApiCatalogoJogos.Controllers.V1
             }
         }
 
+        /// <summary>
+        /// Atualizar o preço de um jogo
+        /// </summary>
+        /// /// <param name="idJogo">Id do jogo a ser atualizado</param>
+        /// <param name="preco">Novo preço do jogo</param>
+        /// <response code="200">Cao o preço seja atualizado com sucesso</response>
+        /// <response code="404">Caso não exista um jogo com este Id</response>   
         [HttpPatch("{idJogo:guid}/preco/{preco:double}")]
         public async Task<ActionResult> AtualizarJogo([FromRoute] Guid idJogo, [FromRoute] double preco)
         {
@@ -90,7 +126,13 @@ namespace ApiCatalogoJogos.Controllers.V1
             }
         }
 
-        [HttpDelete("{idJogo: guid}")]
+        /// <summary>
+        /// Excluir um jogo
+        /// </summary>
+        /// /// <param name="idJogo">Id do jogo a ser excluído</param>
+        /// <response code="200">Cao o preço seja atualizado com sucesso</response>
+        /// <response code="404">Caso não exista um jogo com este Id</response>   
+        [HttpDelete("{idJogo:guid}")]
         public async Task<ActionResult> ApagarJogo([FromRoute] Guid idJogo)
         {
             try
@@ -104,5 +146,6 @@ namespace ApiCatalogoJogos.Controllers.V1
                 return NotFound("Não existe este jogo");
             }
         }
+
     }
 }
--- catalogo-jogos-arquitetura-dotnet/ApiCatalogoJogos/Startup.cs
+++ catalogo-jogos-arquitetura-dotnet/ApiCatalogoJogos/Startup.cs
@@ -11,6 +11,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using ApiCatalogoJogos.Services;
+using ApiCatalogoJogos.Repositories;
 
 namespace ApiCatalogoJogos
 {
@@ -26,7 +28,8 @@ namespace ApiCatalogoJogos
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddScoped<IJogoService, JogoService>();
+            services.AddScoped<IJogoRepository, JogoRepository>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
